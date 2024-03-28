72c752a941f1f7dc36b27ecec7cbed8da0aa090b
Guilherme Santos <133273066+GS0606@users.noreply.github.com>
2024-03-27 21:56:33 -0300

    Add files via upload
    commit de rafatoração das Classes de organização .

--- controller.py
+++ controller.py
@@ -1,25 +1,41 @@
 
-from storage import ClienteStorage
+from flask import Flask, jsonify, request
+from service import ClienteService
 
-class ClienteController:
-    def __init__(self):
-        self.model = ClienteStorage()
+app = Flask(__name__)
+controller = ClienteService()
 
-    def buscar_clientes(self):
-        return self.model.buscar_clientes()
+@app.route('/clientes', methods=['GET'])
+def buscar_clientes():
+    clientes = controller.buscar_clientes()
+    clientes_serializaveis = [cliente.__dict__() for cliente in clientes]
+    return jsonify(clientes_serializaveis)
 
-    def buscar_cliente_por_id(self, id):
-        cliente = self.model.buscar_cliente_por_id(id)
-        if cliente:
-            return cliente
-        else:
-            raise ValueError("Cliente não encontrado")
+@app.route('/clientes/<int:id>', methods=['GET'])
+def buscar_cliente_por_id(id):
+    try:
+        cliente = controller.buscar_cliente_por_id(id)
+        cliente_serializavel = cliente.__dict__()
+        return jsonify(cliente_serializavel)
+    except ValueError as e:
+        return jsonify({"mensagem": str(e)}), 404
 
-    def inserir_cliente(self, novo_cliente):
-        self.model.inserir_cliente(novo_cliente)
+@app.route('/clientes', methods=['POST'])
+def inserir_cliente():
+    novo_cliente = request.get_json()
+    controller.inserir_cliente(novo_cliente)
+    return jsonify({"mensagem": "Cliente inserido com sucesso"})
 
-    def editar_cliente(self, id, cliente_alterado):
-        self.model.editar_cliente(id, cliente_alterado)
+@app.route('/clientes/<int:id>', methods=['PUT'])
+def editar_cliente(id):
+    cliente_alterado = request.get_json()
+    controller.editar_cliente(id, cliente_alterado)
+    return jsonify({"mensagem": "Cliente atualizado com sucesso"})
 
-    def excluir_cliente(self, id):
-        self.model.excluir_cliente(id)
+@app.route('/clientes/<int:id>', methods=['DELETE'])
+def excluir_cliente(id):
+    controller.excluir_cliente(id)
+    return jsonify({"mensagem": "Cliente excluído com sucesso"})
+
+if __name__ == '__main__':
+    app.run(port=5000, host='localhost', debug=True)
---
+++ service.py
@@ -0,0 +1,25 @@
+
+from storage import ClienteStorage
+
+class ClienteService:
+    def __init__(self):
+        self.model = ClienteStorage()
+
+    def buscar_clientes(self):
+        return self.model.buscar_clientes()
+
+    def buscar_cliente_por_id(self, id):
+        cliente = self.model.buscar_cliente_por_id(id)
+        if cliente:
+            return cliente
+        else:
+            raise ValueError("Cliente não encontrado")
+
+    def inserir_cliente(self, novo_cliente):
+        self.model.inserir_cliente(novo_cliente)
+
+    def editar_cliente(self, id, cliente_alterado):
+        self.model.editar_cliente(id, cliente_alterado)
+
+    def excluir_cliente(self, id):
+        self.model.excluir_cliente(id)
